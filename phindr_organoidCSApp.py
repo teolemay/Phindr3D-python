ea7b4bd24eb4d5b2bae790b1dcfd4bf10b5a548b
teolemay <84033812+teolemay@users.noreply.github.com>
2022-06-15 16:31:14 -0400

    extra room around segmentation
    dilation around labels to stop objects being clipped at edges

--- phindr_organoidCSApp.py
+++ phindr_organoidCSApp.py
@@ -280,9 +280,7 @@ def getSegmentedOverlayImage(final_im, min_area_spheroid, radius_spheroid, smoot
         for l in labels[np.nonzero(ii)]:
             L[L==l] = 0
     L = resetLabelImage(L)
-    seg_image = bwperim((L >= 1))
-    seg_image = morph.dilation(seg_image, footprint=np.ones((3,3)))
-    return seg_image, L
+    return L 
 
 
 #   getfsimage.m  
@@ -312,21 +310,12 @@ def getfsimage(imdata, channel):
         prevImage = np.maximum(tmp, prevImage)
     return finalImage, focusIndex
 
-#in same file: #never called hough, not sure why we have it.
-def binImage(I, bsize):
-    m, n = I.shape
-    B1 = ph.im2col(I, (bsize, bsize), type='distinct')
-    B1 = np.mean(B1, axis=0) # I hope this is the right direction along which to take the mean, but I am not sure.
-    im = np.reshape(B1, (int(m/bsize), int(n/bsize))) #if this doesnt work and throws an error, it is probably because the mean was taken along the wrong axis.
-    return im
-
 #   removeBorderObjects.m
 def removeBorderObjects(L, dis):
     """
     remove objects touching border of image
     """
     borderimage = np.zeros(L.shape)
-    #some very strange indexing here, need to reference matlab to numpy guide.
     borderimage[:, :dis] = 1
     borderimage[:, -dis:] = 1
     borderimage[:dis, :] = 1
@@ -351,7 +340,7 @@ def resetLabelImage(L):
     for i, label in enumerate(uL):
         ii = (L == label)
         L[ii] = i+1
-    return L.astype(int)
+    return L.astype('uint8')
 
 #   segmentImage.m
 def segmentImage(imfilename, minArea):
@@ -375,15 +364,7 @@ def segmentImage(imfilename, minArea):
         areaVal = np.sum(ii)
         if areaVal < minArea:
             L[ii] = 0
-    ##stuff below is unused, so I commented it out.
-    # N = np.unique(L)
-    # N = N[N>0] #get rid of background label
-    # for i in range(0, N.size):
-    #     ii = L==N[i]
-    #     L[ii] = i+1
-    # N = np.unique(L)
-    # N = np.sum(N>0) #number of labels above 0
-    return L#, N
+    return L
 
 def smoothImage(IM, cutOff):
     #smooths image in frequency domain
@@ -403,58 +384,6 @@ def smoothImage(IM, cutOff):
     x[x<0] = 0
     return np.abs(np.fft.ifft2(np.fft.fftshift(np.fft.fft2(IM))*x))
 
-
-
-
-#NOT FINISHED WITH THIS ONE YET.
-#   organoidSegmentation.m
-def organoidSegmentation():
-    """
-    this file contains a stand-alone script to wite [sic] out oncogene images
-    calls the other functions in the folder. could probably be put in a "if __name__ == '__main__' " block.
-    leave alone for now, will come back to it later.
-    """
-    return None
-
-
-#another degenrate function: #probably don't need this really. metadata format should always be consistent and easy to hard-code parsing
-#   ParseMetadataFile.m
-# def parseMetaDataFile(metadatafilename):
-#     """called in getPixelBinCenters"""
-#     mData = []
-#     chanInfo = {} #since we call attributes/cell labels: try to use dictionary
-#     header = [] #all three of these are set up as cells
-#     try:
-#         with open(metadatafilename, mode='r+') as fid: #no truncation
-#             header = fid.readline().strip() #first line with leading and trailing whitespaces removed.
-#         header = regexpi(header, r'\t', fmt='split')
-#         if header != 'ImageID':
-#             print('\nERROR: please choose appropriate metadata file. \nThings will probably crash after this.\n\n')
-#             return None
-#         with open(metadatafilename) as fid:
-#             tmp = np.loadtxt(fid, delimiter='\t', skiprows=1, dtype={'names': [f'col{i}' for i in range(0, header.size)], 'formats': ['S4' for i in range(0, header.size)]} )
-#     except Exception as e:
-#         print(e)
-#         print('\nERROR reading metadata file did not work.\n\n')
-#     for i in range(0, tmp.shape[1]):
-#         mData.append(tmp[0, i]) #may or may not work properly.
-#     chanInfo['channelColNumber'] = input('Select channels (type indices of choice separated by spaces) :').split() #list dialog box pops up. shows options of list of header entries, allows multiple selection mode, prompts to select channels. returns indices of selected channels in header.
-#     chanInfo['channelColNumber'] = np.array([int(elem) for elem in chanInfo['channelColNumber']]) #array form of list of str numbers converted to int. (lets try this for now, seems close enough)
-#     chanInfo['channelNames'] = header[0, chanInfo['channnelColNumber']]
-#     chanInfo['channelColors'] = [f'color{i+1}' for i in range(0, chanInfo['channelColNumber'].size)]
-#     ii = header == 'ImageID' #should be case insensitive though
-#     imageID = mData[:, ii].astype(np.float64)
-#     stackCol = np.logical_or((header == 'Stacks'), (header == 'Stacks')) #should also be case insensitive unfortunately
-#     uImageID = np.unique(imageID)
-#     for i in range(0, uImageID.size):
-#         ii = imageID == uImageID[i]
-#         kk = mData[ii, stackCol].astype(np.float64)
-#         tmp = mData[ii, :]
-#         kk = np.argsort(kk)
-#         mData[ii, :] = tmp[kk, :]
-#     ii = header == 'ImageID' #also needs to be case insensitive
-#     imageID = mData[:, ii].astype(np.uint8) #hpe this works, might need to do as list comprehension instead
-#     return mData, imageID, header, chanInfo
 
 
 
